ef7b3ddd3284aa87482962101abefc30f36bb702
ChandrahasaReddy2003 <73829674+ChandrahasaReddy2003@users.noreply.github.com>
2023-06-09 10:04:43 +0530

    Update Music player using python.py

--- Music player using python.py
+++ Music player using python.py
@@ -1,58 +1,60 @@
+import tkinter as tk
 import pygame
 import os
 
 pygame.init()
-
-# Set up the display window (optional)
-pygame.display.set_mode((800, 600))
-
-# Set up the directory containing your music files
 music_directory = "D:\music/"
-
-# Get a list of all the music files in the directory
 music_files = [file for file in os.listdir(music_directory) if file.endswith(".mp3")]
 
-# Initialize the pygame mixer module
 pygame.mixer.init()
-
-# Load the first music file
 current_track = 0
 pygame.mixer.music.load(os.path.join(music_directory, music_files[current_track]))
-
-# Play the music
 pygame.mixer.music.play()
 
-# Main loop
-running = True
-while running:
-    print("Music Player Menu:")
-    print("1. Play/Pause")
-    print("2. Next Track")
-    print("3. Previous Track")
-    print("4. Exit")
+# Function to update the display with the current song name
+def update_display(song_name):
+    now_playing_label.configure(text="Now Playing: " + song_name)
 
-    choice = input("Enter your choice (1-4): ")
-
-    if choice == "1":
-        if pygame.mixer.music.get_busy():
-            pygame.mixer.music.pause()
-        else:
-            pygame.mixer.music.unpause()
-    elif choice == "2":
-        current_track = (current_track + 1) % len(music_files)
-        pygame.mixer.music.load(os.path.join(music_directory, music_files[current_track]))
-        pygame.mixer.music.play()
-    elif choice == "3":
-        current_track = (current_track - 1) % len(music_files)
-        pygame.mixer.music.load(os.path.join(music_directory, music_files[current_track]))
-        pygame.mixer.music.play()
-    elif choice == "4":
-        running = False
+def play_pause():
+    if pygame.mixer.music.get_busy():
+        pygame.mixer.music.pause()
+        play_pause_button.configure(text="Play")
     else:
-        print("Invalid choice. Please enter a number from 1 to 4.")
+        pygame.mixer.music.unpause()
+        play_pause_button.configure(text="Pause")
 
-# Quit the pygame mixer module
+def next_track():
+    global current_track
+    current_track = (current_track + 1) % len(music_files)
+    pygame.mixer.music.load(os.path.join(music_directory, music_files[current_track]))
+    pygame.mixer.music.play()
+    update_display(music_files[current_track])
+
+def prev_track():
+    global current_track
+    current_track = (current_track - 1) % len(music_files)
+    pygame.mixer.music.load(os.path.join(music_directory, music_files[current_track]))
+    pygame.mixer.music.play()
+    update_display(music_files[current_track])
+
+window = tk.Tk()
+window.title("Music Player")
+
+now_playing_label = tk.Label(window, text="Now Playing: ")
+now_playing_label.pack(pady=10)
+
+play_pause_button = tk.Button(window, text="Pause", command=play_pause)
+play_pause_button.pack(pady=10)
+
+next_button = tk.Button(window, text="Next", command=next_track)
+next_button.pack(pady=10)
+
+prev_button = tk.Button(window, text="Previous", command=prev_track)
+prev_button.pack(pady=10)
+
+update_display(music_files[current_track])
+
+window.mainloop()
+
 pygame.mixer.quit()
-
-# Quit pygame
 pygame.quit()
